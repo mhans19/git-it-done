efb73f22033f9c50e86480435fe687044c14b23f
mhans19 <mdhansen19@gmail.com>
2020-06-01 18:47:45 -0600

    add error handling with single.js file

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,33 +2,43 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
-var getRepoName = function(){
+var getRepoName = function() {
+    // grab repo name from url query string
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
-}
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
 
 var getRepoIssues = function(repoName) {
     var apiUrl = "https://api.github.com/repos/" + repoName + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
-            response.json().then(function(data) {
-              displayIssues(data);
-          
-              // check if api has paginated issues
-              if (response.headers.get("Link")) {
-                displayWarning(repoName);
-              }
-            });
+        response.json().then(function(data) {
+            displayIssues(data);
+    
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+            displayWarning(repo);
+            }
+        });
+        } else {
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
         }
-        else {
-          alert("There was a problem with your request!");
-        }
-      });
-    };
+    });
+};
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
